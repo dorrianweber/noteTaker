c46620a1ede6eddf24b131ff20f79ee15260ee00
Dorrian Weber <dorrianweber@gmail.com>
2021-03-10 13:25:38 -0800

    Wrote POST route for notes/api. Saving notes function now works correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,32 @@
-// GET /api/notes
-// POST /api/notes
-
 // DEPENDENCIES
 const path = require('path');
 const fs = require('fs');
 
 // ROUTING
 module.exports = (app) => {
-    // HTML GET Requests
+    // HTML GET requests
     app.get('/api/notes', (req, res) => {
+        // Using 'fs' module to grab info from database
         const database = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf-8');
 
-        console.log(database);
-
+        // Return the response as JSON
         res.json(JSON.parse(database));
+    });
+
+    // POST requests
+    app.post('/api/notes', (req, res) => {
+        // Using 'fs' module to grab info from database
+        const database = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf-8');
+
+        // Parses info from database into JSON
+        const parsedData = JSON.parse(database);
+
+        // Pushes new note into parsed data
+        parsedData.push(req.body);
+
+        // Stringifies parsed data, then rewrites database to include new notes
+        fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(parsedData));
+
+        res.json(true);
     });
 };
