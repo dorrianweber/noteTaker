96d1158cec0610d22748791deff4bb1c75039585
Dorrian Weber <dorrianweber@gmail.com>
2021-03-11 17:24:01 -0800

    Deployed to Heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const path = require('path');
 const fs = require('fs');
+const uniqid = require('uniqid');
 
 // ROUTING
 module.exports = (app) => {
@@ -13,13 +14,18 @@ module.exports = (app) => {
         res.json(JSON.parse(database));
     });
 
-    // POST requests
+    // POST request
     app.post('/api/notes', (req, res) => {
         // Using 'fs' module to grab info from database
         const database = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf-8');
 
         // Parses info from database into JSON
         const parsedData = JSON.parse(database);
+
+        const newestNote = req.body;
+
+        // Adds unique id to newly created note
+        newestNote.id = uniqid();
 
         // Pushes new note into parsed data
         parsedData.push(req.body);
